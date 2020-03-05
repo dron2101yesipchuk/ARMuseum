d98231cf05dc8321885bec3147f620825a20c2c8
Andrew Yesipcuk <yesipchuk.andriy@gmail.com>
2020-03-05 13:49:26 +0200

    some modifications to project

--- ARMuseum/ARMuseum/ViewController.swift
+++ ARMuseum/ARMuseum/ViewController.swift
@@ -2,19 +2,24 @@
 //  ViewController.swift
 //  ARMuseum
 //
-//  Created by Dron on 24.02.2020.
+//  Created by Dron on 05.03.2020.
 //  Copyright © 2020 dron. All rights reserved.
 //
 
 import UIKit
+import RealityKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var arView: ARView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
     }
-
-
 }
-
